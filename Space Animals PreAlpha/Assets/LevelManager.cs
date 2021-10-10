c49148c6d62e8d3b3d66bc749b619b71128bf12e
FreeComet <91998540+FreeComet@users.noreply.github.com>
2021-10-10 17:49:03 -0400

    added start menu
    added start menu and made it the default

---
+++ Space Animals PreAlpha/Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour
+{
+   public void StartGame()
+   {
+	SceneManager.LoadScene("SpaceAnimalsFirstBuild");
+   }
+
+   public void LoadMainMenu()
+   {
+        SceneManager.LoadScene("mainMenu");
+   }
+
+   
+   //quits the game
+   public void QuitGame()
+   {
+	Application.Quit();
+   }
+}
