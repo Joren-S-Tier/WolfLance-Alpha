8dd777f92d0c39bf45822eb76f1b89ba17e3d319
FreeComet <91998540+FreeComet@users.noreply.github.com>
2022-05-26 20:57:05 -0400

    changed basic enemy attack logic

--- WolfLance-VS-Alpha/Basic Enemy.gd
+++ WolfLance-VS-Alpha/Basic Enemy.gd
@@ -6,6 +6,8 @@ export var attack = 1
 export var speed = 10
 var target = null
 
+var player_position = null
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -29,7 +31,7 @@ func _on_Area_body_entered(body):
 	
 func _physics_process(delta):
 	if target != null:
-		self.look_at(target.global_transform.origin, Vector3.UP)
+		self.look_at(player_position, Vector3.UP)
 		var direction = self.global_transform.basis.z
 		move_and_slide(direction *speed * -1)
 
@@ -37,4 +39,4 @@ func _physics_process(delta):
 func _on_PlayerDetection_body_entered(body):
 	if(body.is_in_group("Player")):
 		target = body
-		
+		player_position = body.global_transform.origin
