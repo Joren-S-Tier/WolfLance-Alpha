3b831329e884b061589aa3f54e4cbd8a495e9ca7
FreeComet <91998540+FreeComet@users.noreply.github.com>
2022-06-01 21:42:48 -0400

    built out very rough 3D environment

---
+++ WolfLance-VS-Alpha/CameraHolder.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+var touchSpeed = 4.0
+var minLookAngle = 8.0
+var maxLookAngle = 10.0
+
+var mouseDelta = Vector2()
+
+onready var player = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+
+func _process(delta):
+	var rotation = Vector3(mouseDelta.y, mouseDelta.x, 0) * touchSpeed *delta
+	rotation_degrees.x += rotation.x
+	rotation_degrees.x = clamp(rotation_degrees.x, minLookAngle, maxLookAngle)
+	
+	player.rotation_degrees.y -=rotation.y
---
+++ WolfLance-VS-Alpha/Player.gd
@@ -0,0 +1,45 @@
+extends KinematicBody
+
+var moveSpeed = 10.0
+var jumpSpeed = 20.0
+var gravity = 15.0
+
+var velocity = Vector3()
+onready var camera = get_node("CameraHolder")
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.z = 0
+	
+	var input = Vector3()
+	
+	if Input.is_action_pressed("move_right"):
+		input.z += 1
+	if Input.is_action_pressed("move_left"):
+		input.z-= 1
+	if Input.is_action_pressed("move_up"):
+		input.x+= 1
+	if Input.is_action_pressed("move_down"):
+		input.x-= 1
+	input = input.normalized()
+	
+	var direction = (transform.basis.z * input.z + transform.basis.x *input.x)
+	velocity.x = direction.x * moveSpeed
+	velocity.z = direction.z * moveSpeed
+	velocity.y -= gravity * delta
+	
+	
+	if Input.is_action_pressed("boosting") and is_on_floor():
+		#velocity.y = jumpSpeed
+		pass
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+
+
