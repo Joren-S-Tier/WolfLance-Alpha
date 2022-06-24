49e42c4b103f926a8ce441a80486180395b013fb
FreeComet <91998540+FreeComet@users.noreply.github.com>
2022-06-23 22:05:23 -0400

    randomized explosion sound effects

--- WolfLance-VS-Alpha/explosion.gd
+++ WolfLance-VS-Alpha/explosion.gd
@@ -4,12 +4,17 @@ extends Area
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var randomInt = 0
+var options = [1,2,3,4]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start()
 	$startupTimer.start()
+	#randomInt = abs(round(rand_range(1,4)))
+	randomInt = options[randi() % options.size()]
+	
+	print (randomInt)
 
 
 
@@ -32,7 +37,16 @@ func _on_explosion_body_entered(body):
 
 func _on_startupTimer_timeout():
 	var targets = get_overlapping_bodies()
-	$AudioStreamPlayer.play()
+	match randomInt:
+		1:
+			$Explosion1.play()
+		2:
+			$Explosion2.play()
+		3:
+			$Explosion3.play()
+		4:
+			$Explosion4.play()
+		
 	#print (targets)
 	for target in targets:
 		if target.is_in_group(("Enemy")):
